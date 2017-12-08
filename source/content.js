64001ad46680e00833f7d3a2011ec5686d6346bb
Filipe Kiss <eu@filipekiss.com.br>
2017-12-08 10:13:06 -0200

    :sparkles: Inline Instagram Images
    There's room for improvement, but here's the first idea. Simply match
    instagram links using query selectors, fetch and parse the instagram
    post html and append the image to the tweet.
    
    This aims to close sindresorhus/refined-twitter#2

--- source/content.js
+++ source/content.js
@@ -23,6 +23,43 @@ function hideListAddActivity() {
 	$('#stream-items-id .js-activity-list_member_added').css('display', 'none');
 }
 
+function inlineInstagramPosts() {
+	$('a.twitter-timeline-link[data-expanded-url*="//www.instagram.com').each((idx, instagramAnchor) => {
+		const tweetElement = $(instagramAnchor).parents('.js-tweet-text-container');
+		// Don't do anything else if the image is already embedded
+		if ($(tweetElement).siblings('.AdaptiveMedia').length > 0) {
+			return true;
+		}
+		const instagramPostUrl = instagramAnchor.dataset.expandedUrl;
+		const imageRegex = /"display_url": ?"([^"]+)"/g;
+		fetchInstagramPhoto(instagramPostUrl)
+			.then(postHtml => {
+				const matches = imageRegex.exec(postHtml);
+				const instagramImageUrl = matches[1] || null;
+				if (instagramImageUrl) {
+					insertInstagramPhotoInto(tweetElement, instagramImageUrl, instagramPostUrl);
+				}
+			});
+	});
+}
+
+function insertInstagramPhotoInto(element, imageUrl, postUrl) {
+	const tweetImageTemplate = `
+	<div class="AdaptiveMedia">
+		<a href="${postUrl}" target="_blank">
+			<img class="refined-instagram-inline" src="${imageUrl}" />
+		</a>
+	</div>
+	`;
+	element.after(tweetImageTemplate);
+}
+
+async function fetchInstagramPhoto(postUrl) {
+	const response = await fetch(postUrl);
+	const html = response.text();
+	return html;
+}
+
 async function init() {
 	await safeElementReady('body');
 
@@ -54,6 +91,7 @@ function onDomReady() {
 			safely(useNativeEmoji);
 			safely(hideFollowersActivity);
 			safely(hideListAddActivity);
+			safely(inlineInstagramPosts);
 		});
 	});
 }
