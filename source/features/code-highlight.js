e26e2c2a1bf4817d8831cc01ee189eb576f23e81
Tommaso <tptommy1992@gmail.com>
2017-12-09 15:20:40 +0100

    fixes for prismjs bug

--- source/features/code-highlight.js
+++ source/features/code-highlight.js
@@ -1,27 +1,28 @@
 import prism from "prismjs";
+import 'prismjs/components/prism-jsx'
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-git'
 
 export default function() {
 	const postsContent = $(".stream .stream-items > li .js-tweet-text-container > p");
 	const regex = /```(\w.*)([^```]+)```/g;
-
 	postsContent.each((i,el) => {
 		const postContent = $(el).text();
 		const capturingGroup = regex.exec(postContent);
 
 		if (capturingGroup && capturingGroup.length === 3) {
-			const code = capturingGroup[2];
+			const code = capturingGroup[2].replace(/↵/g, "");
 			const selectedLang = capturingGroup[1].toLowerCase();
-			const html = Prism.highlight(code, Prism.languages[selectedLang]);
+			const highlightedCode = prism.highlight(code, prism.languages[selectedLang]);
 			const updatedHtml = `
-				<pre class='RT_highlight'>
+				<pre class='language-${selectedLang}'>
 					<div class='RT_highlight__lang'>
 					${selectedLang}
 					</div>
-					<code class='RT_highlight'>
-						${html}
+					<code class='language-${selectedLang}'>
+						<div>${highlightedCode}</div>
 					</code>
 				</pre>`
-
 			const newContent = postContent.replace(regex, updatedHtml)
 			$(el).html(newContent)
 		}
