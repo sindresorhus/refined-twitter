2d1f0544d165c618d8d3aff4b5574184d4b1dea9
Tommaso <tptommy1992@gmail.com>
2017-12-11 23:43:55 +0100

    lint

--- source/features/code-highlight.js
+++ source/features/code-highlight.js
@@ -8,19 +8,18 @@ import 'prismjs/components/prism-scss';
 
 const supportedLang = new Set(['javascript', 'jsx', 'bash', 'git', 'typescript', 'html', 'css', 'scss']);
 const aliases = {
-	javascript: new Set([ "js" ]),
-	jsx: new Set([ "react", "reactjsx" ]),
-	bash: new Set([ "zsh", "sh", "shell" ])
+	javascript: new Set(['js']),
+	jsx: new Set(['react', 'reactjsx']),
+	bash: new Set(['zsh', 'sh', 'shell'])
 };
 
 function correctLanguage(lang) {
 	if (supportedLang.has(lang)) {
 		return lang;
-	} else {
-		for(let key in aliases) {
-			if (aliases[key].has(lang)) {
-				return key
-			}
+	}
+	for (const key in aliases) {
+		if (aliases[key].has(lang)) {
+			return key;
 		}
 	}
 }
@@ -35,15 +34,13 @@ export default function () {
 	const postsContent = $('.tweet-text');
 
 	postsContent.each((i, el) => {
-		console.log(i, el)
 		const regex = /```(\w.*)([^.*]+)```/g;
 		const postContent = $(el).text();
 		const capturingGroup = regex.exec(postContent);
 
 		if (capturingGroup && capturingGroup.length === 3) {
 			const code = capturingGroup[2];
-			console.log(capturingGroup[1].toLowerCase())
-			const selectedLang = correctLanguage( capturingGroup[1].toLowerCase() );
+			const selectedLang = correctLanguage(capturingGroup[1].toLowerCase());
 			const tweetText = postContent.replace(regex, '');
 
 			if (selectedLang) {
