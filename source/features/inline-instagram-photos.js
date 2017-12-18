67ece390457e8e0b88ad8964c76e9e1904cd4d87
Filipe Kiss <eu@filipekiss.com.br>
2017-12-18 14:42:50 -0200

    Apply chages from PR review

--- source/features/inline-instagram-photos.js
+++ source/features/inline-instagram-photos.js
@@ -3,13 +3,15 @@ import {h} from 'dom-chef';
 const instagramUrls = new Map();
 
 function createPhotoElement(imageUrl, postUrl) {
-	return <div class="AdaptiveMediaOuterContainer">
-			<div class="AdaptiveMedia">
+	return <div class="AdaptiveMedia">
 				<a href={`${postUrl}`} target="_blank" rel="noopener">
 					<img class="refined-twitter_instagram-inline" src={`${imageUrl}`} />
 				</a>
-			</div>
-		</div>;
+			</div>;
+}
+
+function createPhotoContainer() {
+	return <div class="AdaptiveMediaOuterContainer refined-twitter"></div>;
 }
 
 async function getInstagramPhotoUrl(postUrl) {
@@ -24,23 +26,24 @@ export default function () {
 	$('a.twitter-timeline-link[data-expanded-url*="//www.instagram.com').each(async (idx, instagramAnchor) => {
 		const tweetElement = $(instagramAnchor).parents('.js-tweet-text-container');
 		const instagramPostUrl = instagramAnchor.dataset.expandedUrl;
-		const shouldInlinePhoto = tweetElement.attr('data-refined-twitter-inlined-instagram') === undefined;
+		const shouldInlinePhoto = tweetElement.siblings('.AdaptiveMediaOuterContainer').length < 1;
+
 		if (shouldInlinePhoto) {
-			tweetElement.attr('data-refined-twitter-inlined-instagram', 'waiting');
+			tweetElement.after(createPhotoContainer());
 		} else {
 			return;
 		}
+
 		let imageUrl = '';
-		if (instagramUrls.has(instagramPostUrl) === false) {
+
+		if (instagramUrls.has(instagramPostUrl)) {
+			imageUrl = instagramUrls.get(instagramPostUrl);
+		} else {
 			imageUrl = await getInstagramPhotoUrl(instagramPostUrl);
 			instagramUrls.set(instagramPostUrl, imageUrl);
-		} else {
-			imageUrl = instagramUrls.get(instagramPostUrl);
 		}
-		if (shouldInlinePhoto) {
-			const photoElement = createPhotoElement(imageUrl, instagramPostUrl);
-			tweetElement.after(photoElement);
-			tweetElement.attr('data-refined-twitter-inlined-instagram', 'done');
-		}
+
+		const photoElement = createPhotoElement(imageUrl, instagramPostUrl);
+		tweetElement.siblings('.AdaptiveMediaOuterContainer').html(photoElement);
 	});
 }
