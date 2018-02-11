3723659691ba9a40be7329a4cf0b2fca23ed8f07
Federico Brigante <github@bfred.it>
2018-02-11 14:19:42 +0700

    Use updated selector; use simplified query

--- source/features/likes-button-navbar.js
+++ source/features/likes-button-navbar.js
@@ -1,13 +1,12 @@
 import {h} from 'dom-chef';
 
 export default function () {
-	const globalActionsList = $('#global-actions');
-
-	if (globalActionsList.find('.refined-like-button').length > 0) {
+	// Exit if it already exists
+	if (document.querySelector('.refined-twitter_like-button')) {
 		return;
 	}
 
-	globalActionsList.append(
+	$('#global-actions').append(
 		<li>
 			<a role="button" data-nav="favorites" href="/i/likes" class="js-nav js-tooltip js-dynamic-tooltip refined-like-button" data-placement="bottom">
 				<span class="Icon Icon--heart Icon--large"></span>
