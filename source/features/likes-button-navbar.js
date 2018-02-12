d3364e6af431b35240d2c317d91b744744e43afe
Sindre Sorhus <sindresorhus@gmail.com>
2018-02-12 12:28:21 +0700

    Update likes-button-navbar.js

--- source/features/likes-button-navbar.js
+++ source/features/likes-button-navbar.js
@@ -2,14 +2,14 @@ import {h} from 'dom-chef';
 import {safeElementReady} from '../libs/utils';
 
 export default async () => {
-	await safeElementReady($('#global-actions'));
+	const navBar = await safeElementReady('#global-actions');
 
 	// Exit if it already exists
 	if (document.querySelector('.refined-twitter_like-button')) {
 		return;
 	}
 
-	$('#global-actions').append(
+	$(navBar).append(
 		<li>
 			<a role="button" data-nav="favorites" href="/i/likes" class="js-nav js-tooltip js-dynamic-tooltip refined-like-button" data-placement="bottom">
 				<span class="Icon Icon--heart Icon--large"></span>
