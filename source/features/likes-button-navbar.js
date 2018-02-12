7df68e5058364fc06977682142757b50f3986eda
Sindre Sorhus <sindresorhus@gmail.com>
2018-02-12 12:32:11 +0700

    Update likes-button-navbar.js

--- source/features/likes-button-navbar.js
+++ source/features/likes-button-navbar.js
@@ -9,9 +9,9 @@ export default async () => {
 		return;
 	}
 
-	$(navBar).append(
+	navBar.append(
 		<li>
-			<a role="button" data-nav="favorites" href="/i/likes" class="js-nav js-tooltip js-dynamic-tooltip refined-like-button" data-placement="bottom">
+			<a role="button" data-nav="favorites" href="/i/likes" class="js-nav js-tooltip js-dynamic-tooltip refined-twitter_like-button" data-placement="bottom">
 				<span class="Icon Icon--heart Icon--large"></span>
 				<span class="text">Likes</span>
 			</a>
