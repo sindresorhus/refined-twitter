9529adf0bd7241952c6533b87ea11e776373a24d
Sindre Sorhus <sindresorhus@gmail.com>
2018-03-14 23:40:09 +0700

    Upgrade dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 'use strict';
 const path = require('path');
-const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
@@ -9,21 +8,6 @@ module.exports = {
 		background: './source/background',
 		options: './source/options'
 	},
-	plugins: [
-		new webpack.optimize.ModuleConcatenationPlugin(),
-		new CopyWebpackPlugin([{
-			from: '*',
-			context: 'source',
-			ignore: '*.js'
-		}, {
-			from: 'style/*',
-			context: 'source'
-		}, {
-			from: 'node_modules/webextension-polyfill/dist/browser-polyfill.min.js'
-		}, {
-			from: 'node_modules/jquery/dist/jquery.slim.min.js'
-		}])
-	],
 	output: {
 		path: path.join(__dirname, 'distribution'),
 		filename: '[name].js'
@@ -38,5 +22,19 @@ module.exports = {
 				}
 			}
 		]
-	}
+	},
+	plugins: [
+		new CopyWebpackPlugin([{
+			from: '*',
+			context: 'source',
+			ignore: '*.js'
+		}, {
+			from: 'style/*',
+			context: 'source'
+		}, {
+			from: 'node_modules/webextension-polyfill/dist/browser-polyfill.min.js'
+		}, {
+			from: 'node_modules/jquery/dist/jquery.slim.min.js'
+		}])
+	]
 };
