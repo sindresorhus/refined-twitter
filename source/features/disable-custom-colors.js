595e8703746adab020ce18647e19a905ea0fab3c
Jorge Gonzalez <jrgqhd@gmail.com>
2018-04-17 09:22:45 -0400

    Update disable-custom-colors.js

--- source/features/disable-custom-colors.js
+++ source/features/disable-custom-colors.js
@@ -1,6 +1,8 @@
+import {isProfilePage, getUsername} from '../libs/utils';
+
 export default () => {
-	if (document.body.classList.contains('ProfilePage')) {
-		const userStyles = $('head')[0].querySelector('style');
-		document.body.appendChild(userStyles);
+	if (isProfilePage) {
+		const userStyles = document.getElementById(`user-style-${getUsername()}`);
+		document.body.append(userStyles);
 	}
 };
