d1fe7fe36b7373608a387275a42f0ef6ff3b4b8b
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2018-04-17 09:33:56 -0400

    Update disable-custom-colors.js

--- source/features/disable-custom-colors.js
+++ source/features/disable-custom-colors.js
@@ -1,7 +1,7 @@
 import {isProfilePage, getUsername} from '../libs/utils';
 
 export default () => {
-	if (isProfilePage) {
+	if (isProfilePage()) {
 		const userStyles = document.getElementById(`user-style-${getUsername()}`);
 		document.body.append(userStyles);
 	}
