960639cf0f156d88c76cae5d03159a08e03c4c14
Jorge Gonzalez <jrgqhd@gmail.com>
2018-04-17 12:10:29 -0400

    Override old styles after changing your color theme

--- source/features/disable-custom-colors.js
+++ source/features/disable-custom-colors.js
@@ -3,7 +3,9 @@ import {isProfilePage, getUsername} from '../libs/utils';
 export default () => {
 	const targetClass = `user-style-${getUsername()}`;
 	if (isProfilePage() && !document.body.classList.contains(targetClass)) {
-		const userStyles = document.getElementById(targetClass);
+		// An override class is created and persists temporarily after you change your color theme.
+		const override = `user-style-override-${getUsername()}`;
+		const userStyles = document.getElementById(override) || document.getElementById(targetClass);
 		document.body.append(userStyles);
 	}
 };
