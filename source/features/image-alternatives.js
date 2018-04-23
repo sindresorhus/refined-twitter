20d8f8c1936935156613496390ec54a6e43dc602
Boris SCHAPIRA <borisschapira@gmail.com>
2018-04-23 13:34:59 +0200

    change all bindings to const

--- source/features/image-alternatives.js
+++ source/features/image-alternatives.js
@@ -1,16 +1,15 @@
 export default async () => {
 	const imgContainers = document.querySelectorAll('.AdaptiveMedia-photoContainer');
 
-	for (let imgContainer of imgContainers) {
+	for (const imgContainer of imgContainers) {
 		// Exit if it already exists
 		if (imgContainer.querySelector('.refined-twitter_image-alt')) {
 			return;
 		}
 
 		const imgs = imgContainer.querySelectorAll('img');
-		for (let img of imgs) {
-
-			let imgAlt = img.getAttribute('alt');
+		for (const img of imgs) {
+			const imgAlt = img.getAttribute('alt');
 			if(imgAlt) {
 				var altDiv = document.createElement('div');
 				altDiv.className = 'refined-twitter_image-alt';
