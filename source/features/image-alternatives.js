ed49f15f8ff26991b1988c33f192a1adc892dc4b
Boris SCHAPIRA <borisschapira@gmail.com>
2018-04-23 17:11:53 +0200

    continue instead of return when going through a list of containers or images

--- source/features/image-alternatives.js
+++ source/features/image-alternatives.js
@@ -4,18 +4,21 @@ export default async () => {
 	for (const imgContainer of imgContainers) {
 		// Exit if it already exists
 		if (imgContainer.querySelector('.refined-twitter_image-alt')) {
-			return;
+			continue;
 		}
 
 		const imgs = imgContainer.querySelectorAll('img');
 		for (const img of imgs) {
 			const imgAlt = img.getAttribute('alt');
-			if(imgAlt) {
-				var altDiv = document.createElement('div');
-				altDiv.className = 'refined-twitter_image-alt';
-				altDiv.textContent = imgAlt;
-				img.parentNode.prepend(altDiv);
+
+			if(!imgAlt) {
+				continue;
 			}
+
+			var altDiv = document.createElement('div');
+			altDiv.className = 'refined-twitter_image-alt';
+			altDiv.textContent = imgAlt;
+			img.parentNode.prepend(altDiv);
 		}
 	}
 };
