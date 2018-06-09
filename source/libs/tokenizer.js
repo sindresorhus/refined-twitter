48d8bfea12c1ea9f198993df3fe10f2059dc0fb5
Jorge Gonzalez <jrgqhd@gmail.com>
2018-06-09 09:58:24 -0400

    Fix tokenizer

--- source/libs/tokenizer.js
+++ source/libs/tokenizer.js
@@ -1,28 +1,50 @@
 /**
  * This custom tokenizer for remark-parse is needed so that
  * hashtags (#refinedtwitter) and mentions (@sindresorhus) are
- *	not sanitized when the markdown is compiled to HTML.
+ * not sanitized when the markdown is compiled to HTML.
  */
 
-function mentionsAndHashtags(eat, value, silent) {
-	const match = /^(@|#)(\w+)/.exec(value);
+function mentions(eat, value, silent) {
+	const match = /^@(\w+)/.exec(value);
 
 	if (match) {
 		if (silent) {
 			return true;
 		}
 
+		console.log(match);
 		return eat(match[0])({
 			type: 'link',
-			url: 'https://social-network/' + match[1],
+			url: 'https://twitter.com/' + match[1],
 			children: [{type: 'text', value: match[0]}]
 		});
 	}
 }
 
-mentionsAndHashtags.notInLink = true;
-mentionsAndHashtags.locator = (value, fromIndex) =>
+mentions.notInLink = true;
+mentions.locator = (value, fromIndex) =>
 	value.indexOf('@', fromIndex);
+
+function hashTags(eat, value, silent) {
+	const match = /^#(\w+)/.exec(value);
+
+	if (match) {
+		if (silent) {
+			return true;
+		}
+
+		console.log(match);
+		return eat(match[0])({
+			type: 'link',
+			url: 'https://twitter.com/hashtag/' + match[1],
+			children: [{type: 'text', value: match[0]}]
+		});
+	}
+}
+
+hashTags.notInLink = true;
+hashTags.locator = (value, fromIndex) =>
+	value.indexOf('#', fromIndex);
 
 export default function () {
 	const {Parser} = this;
@@ -30,8 +52,10 @@ export default function () {
 	const methods = Parser.prototype.inlineMethods;
 
 	// Add inline tokenizer.
-	tokenizers.mention = mentionsAndHashtags;
+	tokenizers.mention = mentions;
+	tokenizers.hashTag = hashTags;
 
 	// Run it just before `text`.
 	methods.splice(methods.indexOf('text'), 0, 'mention');
+	methods.splice(methods.indexOf('text'), 0, 'hashTag');
 }
