de4b288c5104db23f34248423f55781da89876bb
Sindre Sorhus <sindresorhus@gmail.com>
2018-06-25 21:14:14 +0700

    Update utils.js

--- source/libs/utils.js
+++ source/libs/utils.js
@@ -59,19 +59,18 @@ export const isProfilePage = () => document.body.classList.contains('ProfilePage
 
 export const isOwnProfilePage = () => isProfilePage() && document.body.classList.contains(`user-style-${getUsername()}`);
 
-// LOCALSTORAGE UTILITIES
 export const getFromLocalStorage = value => {
 	try {
 		return browser.storage.local.get(value);
-	} catch (e) {
-		console.error(`Error while fetching ${value ? JSON.stringify(value, null, 2) : 'everything'}: ${e}`);
+	} catch (error) {
+		console.error(`Error while fetching ${value ? JSON.stringify(value, null, '\t') : 'everything'}: ${error}`);
 	}
 };
 
 export const setToLocalStorage = async value => {
 	try {
 		await browser.storage.local.set(value);
-	} catch (e) {
-		console.error(`Error in storing ${JSON.stringify(value, null, 2)} to local storage: ${e}`);
+	} catch (error) {
+		console.error(`Error in storing ${JSON.stringify(value, null, '\t')} to local storage: ${error}`);
 	}
 };
