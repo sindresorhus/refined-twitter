0be41dbbb9228a33e81457bd6ab87a991dfb64aa
Tomma5o <polettitommaso@gmail.com>
2018-10-01 22:58:14 +0200

    linting and progress

--- source/features/multiple-accounts.js
+++ source/features/multiple-accounts.js
@@ -1,43 +1,53 @@
+import {h} from 'dom-chef';
 import { getUsername, getUserImage } from '../libs/utils';
 
 function accountAlreadyExist() {
-    
+
 }
 
 function setLocalStorage(newAccount) {
-    const { localStorage } = window;
-    const existingAccounts = JSON.parse(localStorage.getItem("activeAccounts"));
-    const updatedAccounts = Object.assign(existingAccounts, newAccount);
+	const { localStorage } = window;
+	const existingAccounts = JSON.parse(localStorage.getItem("activeAccounts"));
+	const updatedAccounts = Object.assign(existingAccounts, newAccount);
 
-    localStorage.setItem("activeAccounts", JSON.stringify(updatedAccounts));
+	localStorage.setItem("activeAccounts", JSON.stringify(updatedAccounts));
 }
 
 function getAccessTokens() {
-    return new Promise((res,rej) => {
-        chrome.runtime.sendMessage({message: "reqAccessToken"}, function(response) {
-            const { key, token } = response;
-            res({key, token})
-            return true;
-        });
-    }).catch(e => e);
+	return new Promise((res, rej) => {
+		chrome.runtime.sendMessage({ message: "reqAccessToken" }, function (response) {
+			const { key, token } = response;
+			res({ key, token })
+			return true;
+		});
+	}).catch(e => e);
 }
 
-function createUserData() {
-    const userImage = getUserImage();
-
-    return getAccessTokens().then(data => ({
-        [userName]: {
-            key: data.key,
-            token: data.token,
-            userImage
-        }
-    }))
+function createUserData(userName, userImage) {
+	return getAccessTokens().then(data => ({
+		[userName]: {
+			key: data.key,
+			token: data.token,
+			userImage
+		}
+	}))
 }
 
-const multipleAccounts = function(){
-    const userName = getUsername();
+const multipleAccounts = function () {
+	const userName = getUsername();
+	const userImage = getUserImage();
+	const dropDownMenu = document.querySelector('.dropdown-menu > ul');
 
-    const currentAccount = createUserData().then(data => data);
+	createUserData(userName, userImage).then(userData => {
+		console.log(userData)
+		const profiles = (
+			<li class="user" onClick={() => { document.cookie = `${userData["Tomma5o"].key}=${userData["Tomma5o"].token}`; window.location.reload() }}>
+				<img src={userData["Tomma5o"].userImage} />
+				<span cookieKey={userData["Tomma5o"].key} cookieToken={userData["Tomma5o"].token}>{"Tomma5o"}</span>
+			</li>
+		)
+		dropDownMenu.appendChild(profiles)
+	});
 
 
 }
