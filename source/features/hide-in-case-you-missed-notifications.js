d858e2a57de8e81a9599cce9bf5d0f4683b39735
Brandon Dean <thedeany@gmail.com>
2018-10-29 15:44:18 -0400

    Use better selector to avoid checking text

--- source/features/hide-in-case-you-missed-notifications.js
+++ source/features/hide-in-case-you-missed-notifications.js
@@ -1,5 +1,3 @@
 export default function () {
-	$('.with-contextTweet')
-		.filter((index, tweet) => tweet.textContent.toLowerCase().includes('in case you missed'))
-		.hide();
+	$('[data-component-context="generic_activity_MagicRecFirstDegreeTweetRecap"]').parents('.js-stream-item').hide();
 }
