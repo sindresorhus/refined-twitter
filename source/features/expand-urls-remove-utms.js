ed6bceb63302e40c28a72f1903006faf0af16b30
Paul Leclercq <paul.leclercq@tabmo.io>
2019-01-10 19:46:42 +0100

    clean up code

--- source/features/expand-urls-remove-utms.js
+++ source/features/expand-urls-remove-utms.js
@@ -1,6 +1,6 @@
 function removeUTMs(url){
   if(url.indexOf('utm') !== -1) {
-    return url.replace(/[?&]utm_.*/,'');
+    return url.replace(/[?&#]utm_.*/,'');
   } else {
     return url;
   }
@@ -14,9 +14,7 @@ function expandUrl(item) {
 export default function () {
 	const urls = $('a[data-expanded-url]');
 
-	for (const el of urls) { //for(var i = 0; i < allShortLinks.length; i++){
-		console.error("ça marche???");
-
+	for (const el of urls) {
 		const expandedUrl = el.getAttribute("data-expanded-url");
 		const urlWithOutUtms = removeUTMs(expandedUrl);
 		el.setAttribute("href", urlWithOutUtms);
