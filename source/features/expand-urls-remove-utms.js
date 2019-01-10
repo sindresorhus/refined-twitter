5a2352f0571514e04ff494b5032a349b2ca1e4f7
Paul Leclercq <paul.leclercq@tabmo.io>
2019-01-10 21:56:41 +0100

    lint code

--- source/features/expand-urls-remove-utms.js
+++ source/features/expand-urls-remove-utms.js
@@ -1,17 +1,13 @@
 function removeUTMs(url) {
-  if (url.indexOf('utm') > 0) {
-    return url.replace(/[?&#]utm_.*/, '');
-  } else {
-    return url;
-  }
+	return url.replace(/[?&#]utm_.*/, '');
 }
 
 export default function () {
-  const urls = $('a[data-expanded-url]');
+	const urls = $('a[data-expanded-url]');
 
-  for (const el of urls) {
-    const expandedUrl = el.getAttribute('data-expanded-url');
-    const urlWithOutUtms = removeUTMs(expandedUrl);
-    el.setAttribute('href', urlWithOutUtms);
-  }
+	for (const el of urls) {
+		const expandedUrl = el.getAttribute('data-expanded-url');
+		const urlWithOutUtms = removeUTMs(expandedUrl);
+		el.setAttribute('href', urlWithOutUtms);
+	}
 }
