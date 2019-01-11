62c8faca9233ea9dbb0e88cc8b413fb2bf0dcd25
Paul Leclercq <paul.leclercq@tabmo.io>
2019-01-11 23:10:30 +0100

    lint

--- source/features/expand-urls.js
+++ source/features/expand-urls.js
@@ -1,4 +1,4 @@
-const refinedTwitterClass = 'refinedTwitterExpandedUrl'; //tag already expanded <a>
+const refinedTwitterClass = 'refinedTwitterExpandedUrl';
 
 function removeUTMs(url) {
 	const parsedURL = new URL(url);
@@ -9,8 +9,8 @@ function removeUTMs(url) {
 export default function () {
 	const urls = $('a[data-expanded-url]:not(.' + refinedTwitterClass + ')');
 	for (const url of urls) {
-		const expandedUrl = url.dataset['expandedUrl'];
-		const urlWithOutUtms = removeUTMs(expandedUrl);
+		let { expandedUrl: urlToExpand } = url.dataset;
+		const urlWithOutUtms = removeUTMs(urlToExpand);
 		url.setAttribute('href', urlWithOutUtms);
 		$(url).addClass(refinedTwitterClass);
 	}
