308908c0d4386b647b1d1efd0b968f786b821d9f
Paul Leclercq <paul.leclercq@tabmo.io>
2019-01-11 20:37:20 +0100

    refacto: use dataset to change html attribute

--- source/features/expand-urls.js
+++ source/features/expand-urls.js
@@ -1,16 +1,15 @@
-
+const refinedTwitterClass = 'refinedTwitterExpandedUrl'; //tag already expanded <a>
 
 function removeUTMs(url) {
-	return url.replace(/[?&#]utm_.*/, '');
+  return url.replace(/[?&#]utm_.*/, '');
 }
 
 export default function () {
-	const refinedTwitterClass = 'refinedTwitterExpandedUrl'; //tag already expanded <a>
-	const urls = $('a[data-expanded-url]:not(.' + refinedTwitterClass +')');
-	for (const url of urls) {
-		const expandedUrl = url.getAttribute('data-expanded-url');
-		const urlWithOutUtms = removeUTMs(expandedUrl);
-		url.setAttribute('href', urlWithOutUtms);
-		$(url).addClass(refinedTwitterClass);
-	}
+  const urls = $('a[data-expanded-url]:not(.' + refinedTwitterClass +')');
+  for (const url of urls) {
+    const expandedUrl = url.dataset.expandedUrl;
+    const urlWithOutUtms = removeUTMs(expandedUrl);
+    url.dataset.href = urlWithOutUtms;
+    $(url).addClass(refinedTwitterClass);
+  }
 }
