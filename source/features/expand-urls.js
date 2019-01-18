5e63ef631f07716bc5cf064ed7680963b25b9187
Paul Leclercq <paul.leclercq@tabmo.io>
2019-01-18 21:16:22 +0100

    wip: use ky to fetch link peeler app

--- source/features/expand-urls.js
+++ source/features/expand-urls.js
@@ -1,4 +1,39 @@
+import ky from 'ky';
+
 const refinedTwitterClass = 'refinedTwitterExpandedUrl';
+
+async function getUnshortenUrl(shortURL) {
+	console.log("getUnshortenUrl " + shortURL);
+
+	const json = await ky.get('https://linkpeelr.appspot.com/api?action=peel_all&url=' + shortURL + '&where=twitter.com&version=2.0.3',
+		{mode:'no-cors'});
+
+	return manageAPIResponse(json, shortURL);
+}
+
+//@TODO
+function parseAPIResponse(apiReponse) {
+	return ["TO", "DO"]
+}
+
+function isAPIReponseValid(apiCode) {
+	return apiCode === '301';
+}
+
+async function manageAPIResponse(apiReponse, shortURL) {
+	console.log("ky response always status 0", apiReponse);
+	console.log("shortURL", shortURL);
+	if(isAPIReponseValid(apiReponse.status)) {
+		const apiReponseParsed = parseAPIResponse(apiReponse);
+		const apiURL = apiReponseParsed[1];
+		const apiCode = apiReponseParsed[0];
+		const realURL= cleanAPIURL(apiURL);
+		realURL = replaceBackSlash(realURL);
+		return realURL;
+	} else {
+		return shortURL;
+	}
+}
 
 function removeUTMs(url) {
 	const parsedURL = new URL(url);
@@ -6,11 +41,12 @@ function removeUTMs(url) {
 	return urlWithOutUtms;
 }
 
-export default function () {
+export default async function () {
 	const urls = $('a[data-expanded-url]:not(.' + refinedTwitterClass + ')');
 	for (const url of urls) {
 		const {expandedUrl: urlToExpand} = url.dataset;
-		const urlWithOutUtms = removeUTMs(urlToExpand);
+		const unshortenUrl = await getUnshortenUrl(urlToExpand)
+		const urlWithOutUtms = removeUTMs(unshortenUrl);
 		url.setAttribute('href', urlWithOutUtms);
 		$(url).addClass(refinedTwitterClass);
 	}
