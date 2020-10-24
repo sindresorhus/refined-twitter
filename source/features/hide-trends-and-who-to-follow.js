6d1f0be95905a33440077f6577a64983f5d3cd4e
yuanLeeMidori <midori3113133@gmail.com>
2020-10-23 22:27:08 -0400

    Search function gone with 'What's Happening' and 'Who To Follow' boxes

--- source/features/hide-trends-and-who-to-follow.js
+++ source/features/hide-trends-and-who-to-follow.js
@@ -1,6 +1,8 @@
 import elementReady from 'element-ready';
 
 export default async function () {
-	const sidebar = await elementReady('[data-testid=sidebarColumn]');
-	$(sidebar).hide();
+	const trending = await elementReady('[aria-label="Timeline: Trending now"]');
+	$(trending).hide();
+	const whoToFollow = await elementReady('[class="css-1dbjc4n r-1u4rsef r-9cbz99 r-1867qdf r-1phboty r-rs99b7 r-ku1wi2 r-1bro5k0 r-1udh08x"]');
+	$(whoToFollow).hide();
 }
