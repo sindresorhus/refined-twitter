77c3312386ca841bf3e4bf99e855d2397d58946c
yuanLeeMidori <midori3113133@gmail.com>
2020-10-25 19:00:25 -0400

    fix users with other languages issue

--- source/features/hide-trends-and-who-to-follow.js
+++ source/features/hide-trends-and-who-to-follow.js
@@ -1,7 +1,7 @@
 import elementReady from 'element-ready';
 
 export default async function () {
-	const trending = await elementReady('[aria-label="Timeline: Trending now"]');
+	const trending = await elementReady('[class="css-1dbjc4n r-1adg3ll r-1ny4l3l"]');
 	$(trending).hide();
 	const whoToFollow = await elementReady('[class="css-1dbjc4n r-1u4rsef r-9cbz99 r-1867qdf r-1phboty r-rs99b7 r-ku1wi2 r-1bro5k0 r-1udh08x"]');
 	$(whoToFollow).hide();
